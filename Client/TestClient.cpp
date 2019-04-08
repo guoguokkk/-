5b3a94bc138ba85761cc660c01fe5275d55a5176
zhijuan kou <1501056593@qq.com>
2019-04-08 20:29:24 +0800

    发送结构化的网络消息数据

--- Client/TestClient.cpp
+++ Client/TestClient.cpp
@@ -4,6 +4,66 @@
 #include<WinSock2.h>
 #define PORT 8082
 #define CLIENT_IP "127.0.0.1"
+
+enum CMD_LINE
+{
+	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
+	CMD_LOGOUT,
+	CMD_LOGOUT_RESULT,
+	CMD_ERROR
+};
+
+//数据报头部
+struct Header
+{
+	int data_length;//数据长度
+	int cmd;//命令
+};
+
+struct LOGIN :public Header
+{
+	LOGIN()
+	{
+		data_length = sizeof(LOGIN);
+		cmd = CMD_LOGIN;
+	}
+	char _user_name[32];
+	char _user_password[32];
+};
+
+struct LOGIN_RESULT :public Header
+{
+	LOGIN_RESULT()
+	{
+		data_length = sizeof(LOGIN_RESULT);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
+	int result;
+};
+
+struct LOGOUT :public Header
+{
+	LOGOUT()
+	{
+		data_length = sizeof(LOGOUT);
+		cmd = CMD_LOGOUT;
+	}
+	char _user_name[32];
+	char _user_password[32];
+};
+
+struct LOGOUT_RESULT :public Header
+{
+	LOGOUT_RESULT()
+	{
+		data_length = sizeof(LOGOUT_RESULT);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
+	int result;
+};
 int main()
 {
 	WORD _version = MAKEWORD(2, 2);
@@ -22,18 +82,47 @@ int main()
 	_client_addr.sin_addr.S_un.S_addr = inet_addr(CLIENT_IP);//inet_addr要关闭SDL
 	_client_addr.sin_port = htons(PORT);
 	_client_addr.sin_family = AF_INET;
-	int _server_sock = connect(_client_sock, (sockaddr*)& _client_addr, sizeof(_client_addr));
-	if (_server_sock < 0)
+	if (connect(_client_sock, (sockaddr*)& _client_addr, sizeof(_client_addr)) < 0)
 		std::cout << "Client connect error. " << std::endl;
 	else
 		std::cout << "Client connect build. " << std::endl;
 
-	//接收服务器信息 recv
-	char _recv_buf[128];
-	if (recv(_client_sock, _recv_buf, sizeof(_recv_buf), 0) < 0)
-		std::cout << "Client recv error. " << std::endl;
-	else
-		std::cout << "Data: " << _recv_buf << std::endl;
+	//发送命令给服务器 send	
+	while (true)
+	{
+		std::string _cmd_buf;
+		std::cin >> _cmd_buf;
+		if (_cmd_buf == "exit")
+		{
+			break;
+		}
+		else if (_cmd_buf == "login")
+		{
+			//向服务器发送请求命令
+			LOGIN _login;
+			strcpy(_login._user_name, "kzj");
+			strcpy(_login._user_password, "123456");
+			send(_client_sock, (const char*)& _login, sizeof(_login), 0);
+
+			//接受服务器返回的数据
+			LOGIN_RESULT _login_result;
+			recv(_client_sock, (char*)& _login_result, sizeof(_login_result), 0);
+			std::cout << "login result: " << _login_result.result << std::endl;
+		}
+		else if (_cmd_buf == "logout")
+		{
+			//向服务器发送请求命令
+			LOGOUT _logout;
+			strcpy(_logout._user_name, "kzj");
+			strcpy(_logout._user_password, "123456");
+			send(_client_sock, (const char*)& _logout, sizeof(_logout), 0);
+
+			//接受服务器返回的数据
+			LOGIN_RESULT _logout_result;
+			recv(_client_sock, (char*)& _logout_result, sizeof(_logout_result), 0);
+			std::cout << "logout result: " << _logout_result.result << std::endl;
+		}
+	}
 
 	//关闭socket  
 	closesocket(_client_sock);
--- Server/TestServer.cpp
+++ Server/TestServer.cpp
@@ -1,9 +1,71 @@
 #define WIN32_LEAN_AND_MEAN//解决<Windows.h>和<WinSock2.h>矛盾
 #include<iostream>
+#include<string>
 #include<Windows.h>
 #include<WinSock2.h>
 #define PORT 8082
 #define SERVER_IP "127.0.0.1"
+
+enum CMD_LINE
+{
+	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
+	CMD_LOGOUT,
+	CMD_LOGOUT_RESULT,
+	CMD_ERROR
+};
+
+//数据报头部
+struct Header
+{
+	int data_length;//数据长度
+	int cmd;//命令
+};
+
+struct LOGIN :public Header
+{
+	LOGIN()
+	{
+		data_length = sizeof(LOGIN);
+		cmd = CMD_LOGIN;
+	}
+	char _user_name[32];
+	char _user_password[32];
+};
+
+struct LOGIN_RESULT :public Header
+{
+	LOGIN_RESULT()
+	{
+		data_length = sizeof(LOGIN_RESULT);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
+	int result;
+};
+
+struct LOGOUT :public Header
+{
+	LOGOUT()
+	{
+		data_length = sizeof(LOGOUT);
+		cmd = CMD_LOGOUT;
+	}
+	char _user_name[32];
+	char _user_password[32];
+};
+
+struct LOGOUT_RESULT :public Header
+{
+	LOGOUT_RESULT()
+	{
+		data_length = sizeof(LOGOUT_RESULT);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
+	int result;
+};
+
 int main()
 {
 	WORD _version = MAKEWORD(2, 2);
@@ -42,12 +104,54 @@ int main()
 	else
 		std::cout << "New Client " << _client_sock << ", ip: " << inet_ntoa(_client_addr.sin_addr) << std::endl;//inet_ntoa
 
-	//向客户端发送一条数据send 
-	char _send_buf[] = { "hello, this is server. " };
-	if (send(_client_sock, _send_buf, sizeof(_send_buf), 0) < 0)
-		std::cout << "Server send error. " << std::endl;
-	else
-		std::cout << "Server send: " << _send_buf << std::endl;
+
+	//服务器接受命令，并返回数据
+	while (true)
+	{
+		Header _header;
+		int _length_header = recv(_client_sock, (char*)& _header, sizeof(_header), 0);
+		if (_length_header <= 0)
+		{
+			std::cout << "cmd is exit" << std::endl;
+			break;
+		}
+		switch (_header.cmd)
+		{
+		case CMD_LOGIN:
+		{
+			std::cout << "cmd is login" << std::endl;
+			//接受客户端命令
+			LOGIN _login;
+			recv(_client_sock, (char*)& _login + sizeof(_header),
+				sizeof(_login) - sizeof(_header), 0);//注意长度
+			std::cout << "username is: " << _login._user_name << " ,password: "
+				<< _login._user_password << std::endl;
+
+			//返回结果
+			LOGIN_RESULT _login_result;
+			send(_client_sock, (const char*)& _login_result, sizeof(LOGIN_RESULT), 0);
+
+		}
+		break;
+		case CMD_LOGOUT:
+		{
+			std::cout << "cmd is logout" << std::endl;
+			//接受客户端命令
+			LOGIN _logout;
+			recv(_client_sock, (char*)& _logout + sizeof(_header),
+				sizeof(_logout) - sizeof(_header), 0);//注意长度
+			std::cout << "username is: " << _logout._user_name << " ,password: "
+				<< _logout._user_password << std::endl;
+
+			//返回结果
+			LOGIN_RESULT _logout_result;
+			send(_client_sock, (const char*)& _logout_result, sizeof(_logout_result), 0);
+			break;
+		}
+		default:
+			break;
+		}
+	}
 
 	//关闭socket  
 	closesocket(_server_sock);
